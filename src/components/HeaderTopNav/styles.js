58539b9364088a4b9e30d0c411e4dea024c66f70
Rodrigo Azevedo <rodrigozcma@gmail.com>
2020-06-30 13:42:56 -0300

    Adicionando icones aos links navBar e adicionando Toggle no user

--- src/components/HeaderTopNav/styles.js
+++ src/components/HeaderTopNav/styles.js
@@ -1,179 +1,184 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
-
-    display: block;
+  display: block;
+  background: white;
+  .max-margin {
+    max-width: 1270px;
+    padding: 0px 30px;
+    margin: 0px auto;
+    @media (max-width: 1100px) {
+      max-width: 992px;
+    }
+  }
+  .top-nav-sizing {
+    width: 100%;
+    height: 80px;
+  }
+  .none {
+    display: none;
+  }
+  .searchBar {
+    width: 55%;
+    margin-left: 0;
+    @media (max-width: 720px) {
+      margin-bottom: 10px;
+      width: 100%;
+    }
+  }
+  .link-top {
+    margin-left: 4px;
+  }
+  a {
+    color: #333;
+    text-decoration: none;
+    cursor: pointer;
+    font-family: "Patua One", cursive;
+  }
+  .display-show {
+    @media (max-width: 720px) {
+      display: none;
+    }
+  }
+  .display-show button {
+    color: white;
+    background-color: #91b431;
+    width: 35px;
+    border-radius: 2.5px;
+    cursor: pointer;
+    transition: background-color 0.4s;
+  }
+  .display-show button:hover {
+    background-color: red;
+  }
+  div.nav-elements-mother {
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    -webkit-box-pack: justify;
+    justify-content: space-between;
+    -webkit-box-align: center;
+    align-items: center;
+    @media (max-width: 956px) {
+      display: flex;
+      flex-direction: column;
+    }
+  }
+  div.under-nav-top {
+    -webkit-box-align: center;
+    align-items: center;
+    border-top: 0.08333rem solid #f2f2f2;
+    -webkit-box-shadow: 0px 2px 18px -9px rgba(0, 0, 0, 0.39);
+    -moz-box-shadow: 0px 2px 18px -9px rgba(0, 0, 0, 0.39);
+    box-shadow: 0px 2px 18px -9px rgba(0, 0, 0, 0.39);
+    @media (max-width: 720px) {
+      display: none;
+    }
+  }
+  ul {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 15px 0 15px 0;
+  }
+  .logo-sizing {
+    height: 72px;
+    padding: 10px 0;
+    @media (max-width: 720px) {
+      height: 50px;
+    }
+  }
+  .button-signout {
+    border: none;
+  }
+  .inputWithIcon {
+    position: relative;
+  }
+  .listbox {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    z-index: 1;
+    position: absolute;
+    list-style: none;
+    background-color: #fff;
+    overflow: auto;
+    max-height: 200px;
+    border: 1px solid rgba(0, 0, 0, 0.25);
+  }
+  .listbox-element {
+    padding: 2px 0 2px 0;
+    display: flex;
+    flex-direction: row;
+    font-weight: 0;
+  }
+  .img-search {
+    max-height: 40px;
+  }
+  .inputWithIcon svg {
+    position: absolute;
+    right: 0;
+    margin-right: 20px;
+    top: 11px;
+    color: #91b431;
+    transition: 0.3s;
+  }
+  .show-when-under-none {
+    -webkit-box-align: center;
+    align-items: center;
+    border-top: 0.08333rem solid #f2f2f2;
+    -webkit-box-shadow: 0px 2px 18px -9px rgba(0, 0, 0, 0.39);
+    -moz-box-shadow: 0px 2px 18px -9px rgba(0, 0, 0, 0.39);
+    box-shadow: 0px 2px 18px -9px rgba(0, 0, 0, 0.39);
+    @media (min-width: 720px) {
+      height: 50px;
+      display: none;
+    }
+  }
+  .flex-icon {
+    display: flex;
+    flex-direction: column;
+    font-size: 10px;
+    align-items: center;
+    font-weight: 300;
+  }
+  .menu-on-scroll {
     background: white;
-    .max-margin{
-        max-width: 1270px;
-        padding: 0px 30px;
-        margin: 0px auto;
-        @media (max-width: 1100px){
-            max-width:992px;
-        }
-    }
-    .top-nav-sizing{
-        width: 100%;
-        height: 80px;
-    }
-    .none{
-        display:none;
-    }
-    .searchBar{
-        width: 55%;
-        margin-left: 0;
-        @media (max-width: 720px){
-            margin-bottom:10px; 
-            width: 100%;
-        }
-    }
-    a{  
-        color: #333;
-        text-decoration: none;
-        cursor: pointer;
-        font-family: 'Patua One', cursive; 
-    }
-    .display-show{
-        @media (max-width: 720px){
-            
-            display:none;
-        }
-        
-    }
-    .display-show button{
-        color: white;
-        background-color:#91B431;
-        width:35px;
-        border-radius: 2.5px;
-        cursor:pointer;
-        transition: background-color 0.4s;
-    }
-    .display-show button:hover{
-        background-color: red;
-    }
-    div.nav-elements-mother{
-        height: 100%;
-        display: flex;
-        flex-direction: row;
-        -webkit-box-pack: justify;
-        justify-content: space-between;
-        -webkit-box-align: center;
-        align-items: center;
-        @media(max-width:956px){
-            display:flex;
-            flex-direction: column;
+    position: fixed;
+    width: 100%;
+    top: 0;
+    box-shadow: 0 -0.83333rem 2.5rem #000;
 
-        }
-        
-        
-    }
-    div.under-nav-top{ 
-        
-        -webkit-box-align: center;
-        align-items: center;
-        border-top: .08333rem solid #f2f2f2;
-        -webkit-box-shadow: 0px 2px 18px -9px rgba(0,0,0,0.39);
-        -moz-box-shadow: 0px 2px 18px -9px rgba(0,0,0,0.39);
-        box-shadow: 0px 2px 18px -9px rgba(0,0,0,0.39);
-        @media (max-width: 720px){
-            display:none;
-        }
-    }
-    ul{
-        display:flex;
-        flex-direction:row;
-        justify-content: space-between;
-        padding: 15px 0 15px 0;
-    }
-    .logo-sizing{
-        height: 72px;
-        padding: 10px 0;
-        @media (max-width: 720px){
-            height: 50px;
-        }
-    }
-    .button-signout{
-        border:none;
-    }
-    .inputWithIcon {
-        position: relative;
-    }
-    .listbox{
-        display: flex;
-        flex-direction: column;
-        width: 100%;
-        margin: 0;
-        padding: 0;
-        z-index: 1;
-        position: absolute;
-        list-style: none;
-        background-color: #fff;
-        overflow: auto;
-        max-height: 200px;
-        border: 1px solid rgba(0,0,0,.25);
-    }
-    .listbox-element{
-        padding: 2px 0 2px 0;
-        display: flex;
-        flex-direction: row;
-        font-weight: 0;
-    }
-    .img-search{
-        max-height: 40px;
-    }
-    .inputWithIcon svg {
-        position: absolute;
-        right: 0;
-        margin-right: 20px;
-        top: 11px;
-        color:#91B431 ;
-        transition: 0.3s;
-    }
-    .show-when-under-none{
-        -webkit-box-align: center;
-        align-items: center;
-        border-top: .08333rem solid #f2f2f2;
-        -webkit-box-shadow: 0px 2px 18px -9px rgba(0,0,0,0.39);
-        -moz-box-shadow: 0px 2px 18px -9px rgba(0,0,0,0.39);
-        box-shadow: 0px 2px 18px -9px rgba(0,0,0,0.39);
-        @media (min-width: 720px){
-            height: 50px;
-            display:none;  
-            
-        }
-    }
-    .flex-icon{
-        display:flex;
-        flex-direction:column;
-        font-size:10px;
-        align-items: center;
-        font-weight:300;    
-        
-    }
-    .menu-on-scroll{
-        background: white;
-        position: fixed;
-         width: 100%;
-         top:0;
-        box-shadow: 0 -0.83333rem 2.5rem #000;
+    z-index: 10;
 
-        z-index:10;
-        
-
-        @media (max-width: 720px){
-           box-shadow:unset; 
-                    
-        }
-}
-.nav-on-scroll{
-    top:100px;
-
-}
-.min-list{
+    @media (max-width: 720px) {
+      box-shadow: unset;
+    }
+  }
+  .nav-on-scroll {
+    top: 100px;
+  }
+  .min-list {
     font-size: 13px;
     color: #333;
     font-weight: 100;
     font-family: Arial, Helvetica, sans-serif;
-}
+  }
+  .undernav-hover {
+    align-items: center;
+    text-align: center;
+  }
+  .undernav-hover:hover {
+    color: green;
+  }
+  .menu-grow {
+    border-radius: 4px;
+    border: solid 1px #ccc;
 
+    -webkit-box-shadow: 1px 1px 3px 0px rgba(0, 0, 0, 0.18);
+    -moz-box-shadow: 1px 1px 3px 0px rgba(0, 0, 0, 0.18);
+    box-shadow: 1px 1px 3px 0px rgba(0, 0, 0, 0.18);
+  }
 `;
